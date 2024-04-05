05c1b3d6c14b4bd552644c296ab914ead016bc43
TrevorCrowell <trevor.crowell1985@gmail.com>
2024-04-04 22:26:22 -0400

    chapter 2 so far

---
+++ chapter2/hello_world.py
@@ -0,0 +1,12 @@
+print("hello world")
+message = "Hello Python world"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+simple_message = "this is a string."
+print(simple_message)
+
+simple_message = "This is also a string"
+print(simple_message)
