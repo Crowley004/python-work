738a2e3b44bbac8aaca48d2a55f0b1b3283133b2
TrevorCrowell <trevor.crowell1985@gmail.com>
2024-04-19 21:54:04 -0400

    Refactored using chain of if statements.

--- chapter5/toppings.py
+++ chapter5/toppings.py
@@ -1,7 +1,17 @@
 # determining whether two values are equal with the inequalitity operator(!=).
 
-requested_toppings = 'mushrooms'
+requested_toppings = ['mushrooms', 'extra cheese']
 
 if requested_toppings != 'anchovies':
     print('hold the anchovies!')
 
+if 'mushrooms' in requested_toppings:
+    print('Adding mushrooms.')
+
+if 'pepperoni' in requested_toppings:
+    print('Adding pepperroni.')
+
+if 'extra cheese' in requested_toppings:
+    print('Adding extra cheese.')
+
+print('\nFinished making your pizza!')
