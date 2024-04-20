8d9f9344d830869a2dcdb24fa403056052a44ff3
TrevorCrowell <trevor.crowell1985@gmail.com>
2024-04-19 23:02:19 -0400

    tiy_5-7

---
+++ chapter5/favorite_fruit.py
@@ -0,0 +1,20 @@
+# 5-7
+# Make a list of three of my favorite fruits.
+# Use five if statements to test if fruit is on the list.
+
+favorite_fruit = ['grapes', 'apples', 'bananas']
+
+if 'grapes' in favorite_fruit:
+    print('I really like grapes.')
+
+if 'peaches' in favorite_fruit:
+    print('I really like peaches.')
+
+if 'apples' in favorite_fruit:
+    print('I really like apples.')
+
+if 'grapefruit' in favorite_fruit:
+    print('I really like grapefruit.')
+
+if 'bananas' in favorite_fruit:
+    print('I really like bananas.')
