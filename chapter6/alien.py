ca25de230ce0a1f8f78443812fc0e6c38379c67e
TrevorCrowell <trevor.crowell1985@gmail.com>
2024-04-25 21:42:29 -0400

    End of day. It was hard to get back in the groove.

--- chapter6/alien.py
+++ chapter6/alien.py
@@ -6,3 +6,27 @@ print(alien_0['points'])
 
 new_points = alien_0['points']
 print(f'You just earned {new_points} points!')
+
+# How to add key-value pairs.
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+# Starting with a empty list.
+
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
+
+# Modifying calues in a dictionary.
+
+alien_0 = {'color': 'green'}
+print(f'The color is {alien_0["color"]}.')
+
+alien_0['color'] = 'yellow'
+print(f'The color is now {alien_0["color"]}.')
