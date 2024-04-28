026ab1b96ad5a4a22df7b7cc173c3cdd2399af05
TrevorCrowell <trevor.crowell1985@gmail.com>
2024-04-28 16:33:38 -0400

    work

---
+++ chapter6/favorite_numbers.py
@@ -0,0 +1,23 @@
+# Use a dictionary to stor people's fav numbers.
+fav_numbers = {
+  'jim': 69,
+  'bill': 34,
+  'bob': 44, 
+  'greg':55,
+  'amber': 12,
+}
+
+num = fav_numbers['jim']
+print(f"Jim's favorit number is {num}.")
+
+num = fav_numbers['bill']
+print(f"Bill's favorit number is {num}.")
+
+num = fav_numbers['bob']
+print(f"Bob's favorit number is {num}.")
+
+num = fav_numbers['greg']
+print(f"Greg's favorit number is {num}.")
+
+num = fav_numbers['amber']
+print(f"Amber's favorit number is {num}.")
