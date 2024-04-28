c84e1bb4193adc461c3dac5c5102ae1599f048f1
TrevorCrowell <trevor.crowell1985@gmail.com>
2024-04-28 15:05:32 -0400

    start of day

---
+++ chapter6/alien_no_points.py
@@ -0,0 +1,4 @@
+# Using the get() method to return a default value if key is doesn't exist.
+alien_0 = {'color': 'green', 'speed': 'slow'}
+point_value = alien_0.get('points'), 'No point value assigned.'
+print(point_value)
