a988a6c052b54417bb978fe5e45e16cc71fc10dd
TrevorCrowell <trevor.crowell1985@gmail.com>
2024-04-28 16:44:32 -0400

    work

---
+++ chapter6/glossary.py
@@ -0,0 +1,24 @@
+# Make a dictionary of five python terms and print.
+
+glossary = {
+    'string': 'A series of characters.',
+    'comment': 'A note in a program that the Python interpreter ignores.',
+    'list': 'A set of items in a particular order.',
+    'loop': 'Work through a set of items, one at a time.',
+    'dictionary': "A set of key-value pairs.",
+    }
+
+word = 'string'
+print(f"\n{word}: {glossary[word]}")
+
+word = 'comment'
+print(f"\n{word}: {glossary[word]}")
+
+word = 'list'
+print(f"\n{word}: {glossary[word]}")
+
+word = 'loop'
+print(f"\n{word}: {glossary[word]}")
+
+word = 'dictionary'
+print(f"\n{word}: {glossary[word]}")
