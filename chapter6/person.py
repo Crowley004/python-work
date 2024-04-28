07f1ada8d89788ca6c61a4b92931f24a8fa23add
TrevorCrowell <trevor.crowell1985@gmail.com>
2024-04-28 15:20:44 -0400

    make a simple dictionary

---
+++ chapter6/person.py
@@ -0,0 +1,11 @@
+# Use a  dictionary to store information about a person.
+person = {
+    'first_name': 'jon',
+    'last_name': 'doe',
+    'age': 25, 
+    'city': 'dayton',
+    }
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
