d0d4b24f28bd8fc960b012e8113448d0998d03b7
Trevor Crowell <crowley004@gmail.com>
2024-05-01 19:29:44 -0400

    update with a for loop

--- chapter6/favorite_languages.py
+++ chapter6/favorite_languages.py
@@ -9,3 +9,8 @@ favorite_languages = {
 
 language = favorite_languages['sarah'].title()
 print(f"Sarah's favorite language is {language}.")
+
+# Using a for loop to get key-values.
+
+for name, language in favorite_languages.items():
+    print(f"\n{name.title()}'s favorite language is {language.title()}.")
