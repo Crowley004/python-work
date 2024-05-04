aa4a4fe05fd1331d958e0f489f97ae86648edae4
Trevor Crowell <crowley004@gmail.com>
2024-05-03 22:27:08 -0400

    I did an update

--- chapter6/glossary.py
+++ chapter6/glossary.py
@@ -6,6 +6,11 @@ glossary = {
     'list': 'A set of items in a particular order.',
     'loop': 'Work through a set of items, one at a time.',
     'dictionary': "A set of key-value pairs.",
+    'key': 'The first item in a key-value pair in a dictionary.',
+    'value': 'An item associated with a key in a dictionary.',
+    'conditional test': 'A comparison between two values.',
+    'float': 'A numerical value with a decimal component.',
+    'boolean expression': 'An expression that evaluates to True or False.',
     }
 
 word = 'string'
@@ -22,3 +27,8 @@ print(f"\n{word}: {glossary[word]}")
 
 word = 'dictionary'
 print(f"\n{word}: {glossary[word]}")
+
+#  Now to clean up the code.
+
+for word, definition in glossary.items():
+    print(f'{word.title()}: {definition}')
