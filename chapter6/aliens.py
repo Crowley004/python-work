af9971f88cd2ff05b107419faddb43e13120d9b5
Trevor Crowell <trevor.crowell1985@gmail.com>
2024-05-06 21:24:23 -0400

    work

--- chapter6/aliens.py
+++ chapter6/aliens.py
@@ -10,4 +10,17 @@ aliens = [alien_0, alien_1, alien_2]
 
 for alien in aliens:
     print(alien)
-print
+
+
+# Make an empty list for storing aliens.
+
+aliens = []
+
+# make 30 green aliens.
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+print('...')
+
+# Show how many aliens have been created.
+print(f'Total numberof aliens: {len(aliens)}')
