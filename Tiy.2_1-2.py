946e2f57f973d21a893c8caf040af5f7cd1a7a2b
Trevor Crowell <Trevor.Crowell1985@gmail.com>
2025-04-01 21:37:57 -0400

    here we go

---
+++ Tiy.2_1-2.py
@@ -0,0 +1,14 @@
+# Assign a sessage to a variable, and print.
+
+message = "I hope I can stick with learning python this time."
+print(message)
+
+# Change the message variable and print.
+
+message = "I am happy my wife is doing this with me!"
+print(message)
+
+
+
+
+
