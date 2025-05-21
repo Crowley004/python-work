2c6180031619a2a9d76409d4c8ac7aaaccb6ea87
Trevor Crowell <trevor.crowell1985@gmail.com>
2025-05-20 21:21:27 -0400

    not much

---
+++ chapter3/cars.py
@@ -0,0 +1,13 @@
+cars = ["bmw", "audi", "toyota", "subaru"]
+cars.sort()
+print(cars)
+
+cars = ["bmw", "audi", "toyota", "subaru"]
+print("Here is the original list:")
+print(cars)
+
+print("\nHere if the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
