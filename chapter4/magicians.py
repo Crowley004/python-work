1d540270c7fe9968f02cbb01f432100ffc4c3983
Trevor Crowell <trevor.crowell1985@gmail.com>
2025-05-25 22:33:51 -0400

    worked late

---
+++ chapter4/magicians.py
@@ -0,0 +1,9 @@
+magicians = ["alice", "david", "carolina"]
+
+for magician in magicians:
+    print(f"\n{magician.title()}, that was a great trick!")
+    print(f"I cant't wait to see your next trick, {magician.title()}.")
+
+print("\nThank you, everyone. That was a great magic show!")
+
+
