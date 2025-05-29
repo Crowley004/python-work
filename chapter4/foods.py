52155b87e5d6abee4b9e4aabc3bbde44d0de1e3d
Trevor Crowell <trevor.crowell1985@gmail.com>
2025-05-29 23:50:44 +0000

    I worked today...

--- chapter4/foods.py
+++ chapter4/foods.py
@@ -1,5 +1,11 @@
 my_foods = ["pizza", "falafel", "carrot cake"]
 friend_foods = my_foods[:]
 
-print("My favorit foods are:")
+my_foods.append("cannoli")
+friend_foods.append("ice cream")
+
+print("My favorite foods are:")
 print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
