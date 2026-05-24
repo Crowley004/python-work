d9cd0ac76c5ac55a04e6692059a138c5f4b44ff5
Trevor Crowell <trevor.crowell1985@gmail.com>
2026-05-23 20:59:28 -0400

    fun

--- chapter6/alien.py
+++ chapter6/alien.py
@@ -1,4 +1,18 @@
 alien_0 = {"color": "green", "points": 5}
 
 print(alien_0["color"])
-print(alien_0["points"])
+print(alien_0["points"]) 
+
+new_point = alien_0["points"]
+print(f"You just earned {new_point} points")
+
+alien_0["x_position"] = 0
+alien_0["y_position"] = 25
+print(alien_0)
+
+alien_1 ={}
+
+alien_1["color"] = "green"
+alien_1["points"] = 5
+
+print(alien_1)
